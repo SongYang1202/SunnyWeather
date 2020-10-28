3902a79f20331e81757c53435bde4c333e7c4b7e
SY <550757@gree.com>
2020-10-28 08:48:48 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/sunnyweather/db/Country.java
@@ -0,0 +1,42 @@
+package com.example.sunnyweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private String cityId;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
